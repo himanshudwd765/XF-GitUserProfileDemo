4e52e65bbc9b94363ac3f3aa0ef8091c40e146a5
Himanshu Dwivedi <himanshudwd765@gmail.com>
2018-01-27 11:19:58 +0530

    Indentation improved

--- GitRepUserList/Models/User.cs
+++ GitRepUserList/Models/User.cs
@@ -5,20 +5,20 @@ namespace GitRepUserList.Models
 {
     public class User
     {
-		[JsonProperty("login")]
-		public string LoginId { get; set; }
+	[JsonProperty("login")]
+	public string LoginId { get; set; }
 
         [JsonProperty("name")]
         public string Name { get; set; }
 
-		[JsonProperty("avatar_url")]
-		public string AvatarUrl { get; set; }
+	[JsonProperty("avatar_url")]
+	public string AvatarUrl { get; set; }
 
-		[JsonProperty("bio")]
-		public string Bio { get; set; }
+	[JsonProperty("bio")]
+	public string Bio { get; set; }
 
-		[JsonProperty("repos_url")]
-		public string ReposUrl { get; set; }
+	[JsonProperty("repos_url")]
+	public string ReposUrl { get; set; }
 
         [JsonIgnore]
         public string Message { get; set; } = string.Empty;
